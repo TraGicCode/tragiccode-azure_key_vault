4889fcef46cd33f140bca4ac3fae9f6bad304103
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2018-12-19 14:26:52 -0600

    (GH-47) Fix ruby 1.9 issue with uri (#48)
    Closes #47

--- lib/puppet_x/tragiccode/azure.rb
+++ lib/puppet_x/tragiccode/azure.rb
@@ -6,7 +6,7 @@ module TragicCode
   class Azure
     def self.get_access_token(api_version)
       uri = URI("http://169.254.169.254/metadata/identity/oauth2/token?api-version=#{api_version}&resource=https%3A%2F%2Fvault.azure.net")
-      req = Net::HTTP::Get.new(uri.to_s)
+      req = Net::HTTP::Get.new(uri.request_uri)
       req['Metadata'] = 'true'
       res = Net::HTTP.start(uri.hostname, uri.port) do |http|
         http.request(req)
@@ -18,7 +18,7 @@ module TragicCode
     def self.get_secret(vault_name, secret_name, vault_api_version, access_token, secret_version)
       version_parameter = secret_version.empty? ? secret_version : "/#{secret_version}"
       uri = URI("https://#{vault_name}.vault.azure.net/secrets/#{secret_name}#{version_parameter}?api-version=#{vault_api_version}")
-      req = Net::HTTP::Get.new(uri.to_s)
+      req = Net::HTTP::Get.new(uri.request_uri)
       req['Authorization'] = "Bearer #{access_token}"
       res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
         http.request(req)

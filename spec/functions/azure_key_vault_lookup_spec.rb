30dbbfd91213d29f42bbc3eb8e32dcfa29f0e19d
michaelf <6224270+TraGicCode@users.noreply.github.com>
2021-04-27 22:16:46 -0500

    Add unit test to ensure addition of key_replacement_token is not a breaking change and will default to '-'

--- spec/functions/azure_key_vault_lookup_spec.rb
+++ spec/functions/azure_key_vault_lookup_spec.rb
@@ -6,7 +6,6 @@ describe 'azure_key_vault::lookup' do
       'vault_name' => 'vault_name',
       'vault_api_version' => 'vault_api_version',
       'metadata_api_version' => 'metadata_api_version',
-      'key_replacement_token' => '-',
     }
   end
   let(:lookup_context) do
@@ -24,7 +23,7 @@ describe 'azure_key_vault::lookup' do
 
   it { is_expected.not_to eq(nil) }
 
-  it 'only accepts 3 arguments' do
+  it 'accepts 3 required arguments' do
     is_expected.to run.with_params.and_raise_error(ArgumentError, %r{expects 3 arguments}i)
   end
   it 'validates the :options hash' do
@@ -58,5 +57,17 @@ describe 'azure_key_vault::lookup' do
     is_expected.to run.with_params(
       'lookup_options', options, lookup_context
     )
+  end
+
+  it 'uses - as the default key_replacement_token' do
+      secret_name = 'profile::windows::sqlserver::sensitive_sql_user_password'
+      access_token_value = 'access_value'
+      secret_value = 'secret_value'
+      expect(TragicCode::Azure).to receive(:normalize_object_name).with(secret_name, '-');
+      expect(TragicCode::Azure).to receive(:get_access_token).and_return(access_token_value)
+      expect(TragicCode::Azure).to receive(:get_secret).and_return(secret_value)
+      is_expected.to run.with_params(
+        'profile::windows::sqlserver::sensitive_sql_user_password', options, lookup_context
+      ).and_return(secret_value)
   end
 end
